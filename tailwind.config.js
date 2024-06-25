6b1ccf5cf61098705a8112ef69160e9035a4f88d
Arjay Malaga <arjaymalaga990@gmail.com>
2024-06-26 03:52:22 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ["./index.html", "./src/**/*.{js,jsx}"],
+  },
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ],
+  mode: "jit",
+  theme: {
+    extend: { 
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        inter: ["Inter", "Poppins"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [
+    require('tailwindcss-animated'),
+    require('taos/plugin'),
+  ],
+};
